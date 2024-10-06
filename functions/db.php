7584fae5fbb1e43bf882f4b660c5f600b879e871
Henry Gross-Hellsen <acorn.misuser.0q@icloud.com>
2024-10-05 17:30:30 -0700

    fix typo in db.php where it'd always assume it's a one-time connection

--- functions/db.php
+++ functions/db.php
@@ -30,20 +30,20 @@ class Db {
 		try {
 		    $testconn = mysqli_connect($_POST['db-host'], $_POST['db-user'], $_POST['db-pass'], $_POST['db-name']);
 		} catch (mysqli_sql_exception $e) {
-		    error_log("Connection test failed: ".$e);
+		    error_log("db.php: test(): Connection test failed: ".$e);
 		    return false;
 		}
 		mysqli_disconnect($testconn);
 		return true; 
 	}
 
-	public function test($host, $user, $pass, $name) {
+	public function test2($host, $user, $pass, $name) {
 		// pure
 		$testconn;
 		try {
 		    $testconn = mysqli_connect($host, $user, $pass, $name);
 		} catch (mysqli_sql_exception $e) {
-		    error_log("Connection test failed: ".$e);
+		    error_log("db.php: test2(): Connection test failed: ".$e);
 		    return false;
 		}
 		mysqli_disconnect($testconn);
@@ -64,9 +64,9 @@ class Db {
 		return true;
 	}
 
-	public function connect($host, $user, $pass, $name) {
+	public function connect2($host, $user, $pass, $name) {
 		if (isset($this->conn)) {
-			error_log("db.php: connect(): already connected!");
+			error_log("db.php: connect2(): already connected!");
 			return true;
 		}
 		try {
@@ -87,7 +87,7 @@ class Db {
 		$one_time_connection=false;
 
 		// one-time connection if $conn isn't set
-		if (!isset($conn)) {
+		if (!isset($this->conn)) {
 			if ($this->connect()) {
 				$one_time_connection=true;
 			} else {
@@ -103,14 +103,14 @@ class Db {
 			$result = mysqli_query($this->conn, $querystring);
 		} catch (mysqli_sql_exception $e) {
 			// die("SQL query exception: ".$e);
-		    error_log("SQL query exception: ".$e);
+		    error_log("db.php: query(): SQL query exception: ".$e);
 		    if ($one_time_connection) $this->disconnect();
 		    return [];
 		}
 
 		if (!$result) {
 			// die("SQL query error: ".mysqli_error($result));
-		    error_log("SQL query error: ".mysqli_error($result));
+		    error_log("db.php: query(): SQL query error: ".mysqli_error($result));
 		    if ($one_time_connection) $this->disconnect();
 		    return [];
 		}
@@ -136,6 +136,7 @@ class Db {
 		error_log("db.php: insert_id(): ".mysqli_insert_id($this->conn));
 		return mysqli_insert_id($this->conn);
 	}
+
 	public function error() {
 		error_log("db.php: error(): ".mysqli_error($this->conn));
 		return mysqli_error($this->conn);
