5e4a109195a6d0d26d46b73df6c0daafbaefba0c
Henry Gross-Hellsen <acorn.misuser.0q@icloud.com>
2024-10-09 18:45:34 -0700

    send_other.php: properly handle filename collisions

--- functions/send_other.php
+++ functions/send_other.php
@@ -10,16 +10,46 @@ if (substr($_SESSION['perms'],3,1)!=="1") {
     exit();
 }
 
+
 $fileName = $_FILES["fiels"]["name"];
 $fileTmpLoc = $_FILES["fiels"]["tmp_name"];
 if (!$fileTmpLoc) {
-    echo '{"status":"error","message":"File is too big! Check your post_max_size (current value '.ini_get('post_max_size').') andupload_max_filesize (current value '.ini_get('upload_max_filesize').') values in '.php_ini_loaded_file().'"}';
+    echo '{"status":"error","message":"File is too big! Check your post_max_size (current value '.ini_get('post_max_size').') and upload_max_filesize (current value '.ini_get('upload_max_filesize').') values in '.php_ini_loaded_file().'"}';
     exit();
 }
 
+// unlike send_mods.php, we don't have any metadata to go off of. 
+// and as we don't create a zip, we can simply check the files' md5s.
+// this means a user can upload multiple timestamp variations of the exact same file, in a zip.
+// we will allow this.
+
+$md5_file_tmp=md5_file($fileTmpLoc);
+
 if (file_exists("../others/".$fileName)) {
-    echo '{"status":"error","message":"File already exists!"}';
-    exit();
+    // immediately check if they're identical
+    if (md5_file("../others/".$fileName)===$md5_file_tmp) {
+        error_log('identical file');
+        die ('{"status":"error","message":"File already exists!"}');
+
+    } else { // file exists, different content! gotta cycle through variations
+        $counter = 1;
+        $tmpName = str_replace('.zip', '-'.$counter.'.zip', $fileName);
+
+        // while filename variation also exists and is different
+        while (file_exists('../others/'.$tmpName) && md5_file('../others/'.$tmpName)!==$md5_file_tmp) {
+            $counter += 1;
+            $tmpName = str_replace('.zip', '-'.$counter.'.zip', $fileName);
+        }
+
+        // if we ended up finding an identical filename variation
+        if (file_exists('../others/'.$tmpName) && md5_file('../others/'.$tmpName)===$md5_file_tmp) {
+            error_log('identical file variation');
+            die ('{"status":"error","message":"File already exists!"}');
+        }
+
+        // settle on a filename variation
+        $fileName = $tmpName;
+    }
 }
 
 require('slugify.php');
@@ -34,7 +64,7 @@ if (move_uploaded_file($fileTmpLoc, "../others/".$fileName)) {
     $pretty_name = $db->sanitize($fileName);
     $name = slugify($pretty_name);
     $author = $config['author'];
-    $url = "http://".$config['host'].$config['dir']."others/".$fileName;
+    $url = strtolower(current(explode('/',$_SERVER['SERVER_PROTOCOL'])))."://".$config['host'].$config['dir']."others/".$fileName;
     $md5 = md5_file("../others/".$fileName);
 
     $res = $db->query("INSERT INTO `mods` (`name`,`pretty_name`,`md5`,`url`,`author`,`description`,`filename`,`type`) VALUES ('".$name."','".$pretty_name."','".$md5."','".$url."','".$author."','Custom file by ".$author."','".$fileName."','other')");
