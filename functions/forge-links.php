a54f78e6b0b61861e2b6fdef50ee4aeec52392bb
Henry Gross-Hellsen <6283258+cowpod@users.noreply.github.com>
2024-10-12 23:04:55 -0700

    Revert "forge-links.php: remove unecessary db query"
    This reverts commit 691b5d910d75c42a1cb76ca05bc372244e1cf5b6.

--- functions/forge-links.php
+++ functions/forge-links.php
@@ -2,8 +2,8 @@
 // this is patchwork in order to make up for missing links in the offical json file.
 // should the links become available, or the format of their files change, this needs to be rewritten.
 
-$forge_json = file_get_contents("https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json"); // can't find normal promotions.json
-if (empty($forge_json)) {
+$forge_data = file_get_contents("https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json"); // can't find normal promotions.json
+if (empty($forge_data)) {
     error_log("forge-links.php: couldn't get forge data");
     die("couldn't get forge data");
 }
@@ -17,13 +17,18 @@ require_once("db.php");
 $db=new Db;
 $db->connect();
 
+// this was called INSIDE the for loop before...
+$forgesq = $db->query("SELECT `version` FROM `mods` WHERE `type` = 'forge'");
 $forges=[];
-$forge_data=json_decode($forge_json, true)['promos'];
+if($forgesq) {
+    foreach($forgesq as $forge) {
+        array_push($forges, $forge['version']);
+    }
+}
 
-foreach ($forge_data as $gameVersion => $forgeVersion) { // key, value
+foreach (json_decode($forge_data, true)['promos'] as $gameVersion => $forgeVersion) { // key, value
     $id++;
-
-    if (str_contains($gameVersion, "latest")) {
+    if (strpos($gameVersion, "latest")) {
         $gameVersion = str_replace("-latest", "", $gameVersion);
         // 1.13 "1.13.2-25.0.219" and later don't have a universal.jar
         if (version_compare($gameVersion, '1.13', '>=')) {
@@ -31,7 +36,6 @@ foreach ($forge_data as $gameVersion => $forgeVersion) { // key, value
         } else {
             $suffixExt = 'universal.jar';
         }
-
         $versions[$forgeVersion] = $forge_link.'/'.$gameVersion.'-'.$forgeVersion.'/forge-'.$gameVersion.'-'.$forgeVersion.'-'.$suffixExt;
 
         if (!in_array($forgeVersion, $forges)) {
