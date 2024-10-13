50eeaa912cae99c5777e92668b57ca76927c92c0
Henry Gross-Hellsen <acorn.misuser.0q@icloud.com>
2024-10-12 17:22:18 -0700

    properly convert fabric version string to interval notation, improve in_range()

--- functions/interval_range_utils.php
+++ functions/interval_range_utils.php
@@ -1,31 +1,58 @@
 <?php
 
+function fabric_to_interval_range($str) {
+	// convert fabric's ">=X <=X" to interval notation
+	$matches=[];
+	if (preg_match("/^(>=|>)(.*)\s(<=|<)(.*)$/", $str, $matches)) { // >=|>... <=|<...
+		[$full,$op1,$num1,$op2,$num2] = $matches;
+		$op1 = ($op1=='>=') ? '[' : '(';
+		$op2 = ($op2=='<=') ? ']' : ')';
+		return $op1.$num1.','.$num2.$op2;
+	}
+	elseif (preg_match("/^(>=|>)(.*)$/", $str, $matches)) { // >=|>...
+		[$full,$op,$num]=$matches;
+		$op = ($op=='>=') ? '[' : '(';
+		return $op.$num.',)';
+	}
+	elseif (preg_match("/^(<=|<)(.*)$/", $str, $matches)) { // <=|<...
+		[$full,$op,$num]=$matches;
+		$op = ($op=='<=') ? ']' : ')';
+		return '(,'.$num.$op;
+	}
+}
+
+// used to trim trailing .0 as php (as of 8.3) believes that 1.2.0 is greater than 1.2
+function removeEnding($string, $remove) {
+    if (substr($string, -strlen($remove)) === $remove) {
+        return substr($string, 0, -strlen($remove));
+    }
+    return $string;
+}
+
 function in_range($range, $num) {
-	// compares two minecraft versions. 
 	// returns true if $num is in interval range $range.
 	// returns false otherwise.
 	
-	if (empty($range)) return false;
-	if (empty($num)) return false;
+	if (empty($range)) return FALSE;
+	if (empty($num)) return FALSE;
+
+	$num=removeEnding($num,'.0');
 
 	$matches=[];
-	if (preg_match("/^(?:\[)?(.*),(.*)(?:\])?$/", $range, $matches)) { // [, ]
-		[$full,$low,$high]=$matches;
+	if     (preg_match("/^(\[)([0-9\.]*),\s?([0-9\.]*)(\])$/", $range, $matches)) { // [, ]
+		[$full,$not1,$low,$high,$not2]=$matches;
 		return (version_compare($num, $low, '>=') && version_compare($num, $high, '<='));
 	}
-	$matches=[];
-	if (preg_match("/^(?:\[)?(.*),(.*)(?:\))?$/", $range, $matches)) { // [, )
-		[$full,$low,$high]=$matches;
+	elseif (preg_match("/^(\[)([0-9\.]*),\s?([0-9\.]*)(\))$/", $range, $matches)) { // [, )
+		[$full,$not1,$low,$high,$not2]=$matches;
 		return (version_compare($num, $low, '>=') && version_compare($num, $high, '<'));
 	}
-	$matches=[];
-	if (preg_match("/^(?:\()?(.*),(.*)(?:\])?$/", $range, $matches)) { // (, ]
-		[$full,$low,$high]=$matches;
+	elseif (preg_match("/^(\()([0-9\.]*),\s?([0-9\.]*)(\])$/", $range, $matches)) { // (, ]
+		[$full,$not1,$low,$high,$not2]=$matches;
 		return (version_compare($num, $low, '>') && version_compare($num, $high, '<='));
 	}
-	$matches=[];
-	if (preg_match("/^(?:\()?(.*),(.*)(?:\))?$/", $range, $matches)) { // (, )
-		[$full,$low,$high]=$matches;
+	elseif (preg_match("/^(\()([0-9\.]*),\s?([0-9\.]*)(\))$/", $range, $matches)) { // (, )
+		[$full,$not1,$low,$high,$not2]=$matches;
 		return (version_compare($num, $low, '>') && version_compare($num, $high, '<'));
 	}
 	
@@ -34,6 +61,7 @@ function in_range($range, $num) {
 }
 
 function mcversion_to_range($mcversion) {
+	//todo: make this like above
     $min=null;
     $max=null;
     $mcvrange=explode(",", trim($mcversion,"[]()"));
