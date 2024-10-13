14bfcce47e1fce67360a9b0a9baec45f3d201471
Henry Gross-Hellsen <6283258+cowpod@users.noreply.github.com>
2024-10-12 23:10:38 -0700

    Update new-modpack.php
    check

--- functions/new-modpack.php
+++ functions/new-modpack.php
@@ -23,15 +23,14 @@ $mpi = array_key_exists('id', $mpq) ? intval($mpq['id'])+1 : 1;
 
 $db->execute("INSERT INTO modpacks(`name`,`display_name`,`icon`,`icon_md5`,`logo`,`logo_md5`,`background`,`background_md5`,`public`) VALUES ('unnamed-modpack-".$mpi."','Unnamed modpack','http://".$config['host'].$config['dir']."resources/default/icon.png','A5EA4C8FA53984C911A1B52CA31BC008','http://".$config['host'].$config['dir']."resources/default/logo.png','70A114D55FF1FA4C5EEF7F2FDEEB7D03','http://".$config['host'].$config['dir']."resources/default/background.png','88F838780B89D7C7CD10FE6C3DBCDD39',1)");
 
-$mpq = $db->query("SELECT `id` FROM `modpacks` ORDER BY `id` DESC LIMIT 1");
+$mpq = $db->query("SELECT id FROM `modpacks` ORDER BY `id` DESC LIMIT 1");
 if ($mpq) {
     assert(sizeof($mpq)==1);
     $mp=$mpq[0];
 }
-$mpi2 = intval($mp['id']);
 
 // if this isn't true then something f'ed up
-assert($mpi==$mpi2);
+assert($mpi==$mp['id']);
 
-header("Location: ".$config['dir']."modpack?id=".$mpi2);
+header("Location: ".$config['dir']."modpack?id=".intval($mp['id']));
 exit();
