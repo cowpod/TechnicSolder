4357afd2d69429e1c4c3a57570d6e17b6d431cdc
Henry Gross-Hellsen <6283258+cowpod@users.noreply.github.com>
2024-10-12 21:38:45 -0700

    update interval_range_utils.php

--- functions/interval_range_utils.php
+++ functions/interval_range_utils.php
@@ -3,22 +3,53 @@
 function fabric_to_interval_range($str) {
 	// convert fabric's ">=X <=X" to interval notation
 	$matches=[];
-	if (preg_match("/^(>=|>)(.*)\s(<=|<)(.*)$/", $str, $matches)) { // >=|>... <=|<...
+	if (preg_match("/^(>=|>)([0-9\.]*)\s(<=|<)([0-9\.]*)$/", $str, $matches)) { // >=|>... <=|<...
 		[$full,$op1,$num1,$op2,$num2] = $matches;
 		$op1 = ($op1=='>=') ? '[' : '(';
 		$op2 = ($op2=='<=') ? ']' : ')';
 		return $op1.$num1.','.$num2.$op2;
 	}
-	elseif (preg_match("/^(>=|>)(.*)$/", $str, $matches)) { // >=|>...
+	elseif (preg_match("/^(>=|>)([0-9\.]*)$/", $str, $matches)) { // >=|>...
 		[$full,$op,$num]=$matches;
 		$op = ($op=='>=') ? '[' : '(';
 		return $op.$num.',)';
 	}
-	elseif (preg_match("/^(<=|<)(.*)$/", $str, $matches)) { // <=|<...
+	elseif (preg_match("/^(<=|<)([0-9\.]*)$/", $str, $matches)) { // <=|<...
 		[$full,$op,$num]=$matches;
 		$op = ($op=='<=') ? ']' : ')';
 		return '(,'.$num.$op;
 	}
+
+	elseif (str_starts_with(strtolower($str), '~')) {
+		if (str_ends_with(strtolower($str),'.x')) // in case it ALSO ends in .x
+			$str=str_replace('.x','',$str);
+		$lower=str_replace('~', '', strtolower($str));
+		$exp=explode('.',$lower);
+		$incremented_last=$exp[sizeof($exp)-1]+1; // increment last
+		array_pop($exp); // remove last
+		array_push($exp, $incremented_last);
+		$upper=implode('.', $exp);
+		return '['.$lower.','.$upper.')';
+	}
+	elseif (str_ends_with(strtolower($str), ".x")) {
+		$lower=str_replace('.x', '', strtolower($str));
+		$exp=explode('.',$lower);
+		$incremented_last=$exp[sizeof($exp)-1]+1; // increment last
+		array_pop($exp); // remove last
+		array_push($exp, $incremented_last);
+		$upper=implode('.', $exp);
+		return '['.$lower.','.$upper.')';
+	}
+
+	if (empty($str))
+		return '*';
+
+	// just to clarify what we consider to be 'any'
+	if ($str=='*') {
+		return '*';
+	}
+
+	return $str;
 }
 
 // used to trim trailing .0 as php (as of 8.3) believes that 1.2.0 is greater than 1.2
@@ -33,8 +64,7 @@ function in_range($range, $num) {
 	// returns true if $num is in interval range $range.
 	// returns false otherwise.
 	
-	if (empty($range)) return FALSE;
-	if (empty($num)) return FALSE;
+	if (empty($range) || empty($num)) return FALSE;
 
 	$num=removeEnding($num,'.0');
 
@@ -60,22 +90,41 @@ function in_range($range, $num) {
 	return version_compare($range,$num,'=');
 }
 
-function mcversion_to_range($mcversion) {
-	//todo: make this like above
+
+function parse_interval_range($mcversion) {
     $min=null;
     $max=null;
-    $mcvrange=explode(",", trim($mcversion,"[]()"));
 
-    if (sizeof($mcvrange)==2) {
-        $min=$mcvrange[0];
-        $max=$mcvrange[1];
-    }
-    elseif (sizeof($mcvrange)==1) {
-        $min=$mcvrange[0];
-        // don't specify a max
-    }
+    // blank or any
+	if ($mcversion=='*'||empty($mcversion)) {
+		return ['min'=>PHP_INT_MIN, 'max'=>PHP_INT_MAX, 'min_inclusivity'=>'>', 'max_inclusivity'=>'<'];
+	}
+    // is it actually interval notation?
+    elseif (str_contains($mcversion, ',') && (str_starts_with($mcversion,'(')||str_starts_with($mcversion,'[')) && (str_ends_with($mcversion,')')||str_ends_with($mcversion,']'))) {
+	    $mcvrange=explode(",", trim($mcversion,"[]()"));
 
-    return ["min"=>$min, "max"=>$max, "min_inclusivity"=>$mcversion[0], "max_inclusivity"=>$mcversion[-1]];
+	    if (sizeof($mcvrange)==2) {
+	        $min=$mcvrange[0];
+	        $max=$mcvrange[1];
+	    }
+	    elseif (sizeof($mcvrange)==1) {
+	        $min=$mcvrange[0];
+	        // don't specify a max
+	    }
+    	return ["min"=>$min, "max"=>$max, "min_inclusivity"=>$mcversion[0]=='[' ? '>=' : '>', "max_inclusivity"=>$mcversion[-1]==']' ? '<=' : '<'];
+	} 
+	// we assume it's a number
+	else {
+		$min=$mcversion;
+		$exp=explode('.',$mcversion);
+		error_log($exp[sizeof($exp)-1]);
+		$incremented_last=$exp[sizeof($exp)-1]+1; // increment last
+		array_pop($exp); // remove last
+		array_push($exp, $incremented_last);
+		$max=implode('.', $exp);
+		return ['min'=>$min, 'max'=>$max, 'min_inclusivity'=>'>=', 'max_inclusivity'=>'<'];
+	}
+
 }
 
 ?>
