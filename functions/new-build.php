7633024aa435708a1bb631a2feeddbf01d9df8a1
Henry Gross-Hellsen <6283258+cowpod@users.noreply.github.com>
2025-01-26 16:37:25 -0800

    new-build.php: sanitize input

--- functions/new-build.php
+++ functions/new-build.php
@@ -10,11 +10,15 @@ if (empty($_GET['id'])) {
     die("Modpack ID not specified.");
 }
 if (empty($_GET['name'])) {
-    die("Name not specified.");
+    die("Build name not specified.");
 }
-if (empty($_GET['type'])) {
-    die("type not specified.");
+if (!is_numeric($_GET['id'])) {
+    die("Malformed id");
 }
+if (!preg_match('/[\w\-\.]+/',$_GET['name'])) {
+    die("Malformed name");
+}
+
 if (!$_SESSION['user']||$_SESSION['user']=="") {
     die("Unauthorized request or login session has expired!");
 }
@@ -26,15 +30,18 @@ require_once("db.php");
 $db=new Db;
 $db->connect();
 
-$nameexistsq = $db->query("SELECT 1 FROM builds WHERE name = '{$db->sanitize($_GET['name'])}' AND modpack = {$db->sanitize($_GET['id'])} LIMIT 1");
+$name = strtolower($_GET['name']);
+$id = $_GET['id'];
+
+$nameexistsq = $db->query("SELECT 1 FROM builds WHERE name = '{$name}' AND modpack = {$id} LIMIT 1");
 if ($nameexistsq) {
     die("Build with name {$_GET['name']} already exists");
 }
-$addbuild = $db->execute("INSERT INTO builds(name,modpack,public) VALUES ('{$db->sanitize($_GET['name'])}', '{$db->sanitize($_GET['id'])}', 0)");
+$addbuild = $db->execute("INSERT INTO builds(name,modpack,public) VALUES ('{$name}', '{$id}', 0)");
 if (!$addbuild) {
     die("Could not add build.");
 }
 
 $db->disconnect();
-header("Location: ".$config->get('dir')."modpack?id=".$_GET['id']);
+header("Location: {$config->get('dir')}modpack?id={$id}");
 exit();
