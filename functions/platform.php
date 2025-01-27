1439a6d132ab2f85e73628841fba8051d7b9d68d
Henry Gross-Hellsen <6283258+cowpod@users.noreply.github.com>
2025-01-26 16:02:57 -0800

    Update platform.php
    address no output when neither slug nor build are passed
    sanitize input

--- functions/platform.php
+++ functions/platform.php
@@ -1,12 +1,15 @@
 <?php
 header('Content-Type: application/json');
-if (isset($_GET['slug'])&&isset($_GET['build'])) {
-	@$data=file_get_contents("http://api.technicpack.net/modpack/".$_GET['slug']."?build=".$_GET['build']);
-	if ($data) {
-		echo $data;
-	} else {
-		echo "{}";
-	}
+if (!isset($_GET['slug'])||!isset($_GET['build'])) {
+	die("{}");
+}
+if (!preg_match('/[\w\-_]+/',$_GET['slug'])||!preg_match('/[\w\.\-_]+/',$_GET['build'])) {
+	die("{}");
 }
 
-exit();
+@$data=file_get_contents("http://api.technicpack.net/modpack/".$_GET['slug']."?build=".$_GET['build']);
+if ($data) {
+	die($data);
+} else {
+	die("{}");
+}
