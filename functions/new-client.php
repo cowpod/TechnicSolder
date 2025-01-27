3ed7277371d1ed444f41aec04a3fd0259b6d10b8
Henry Gross-Hellsen <6283258+cowpod@users.noreply.github.com>
2025-01-26 16:10:54 -0800

    Update new-client.php
    sanitize input

--- functions/new-client.php
+++ functions/new-client.php
@@ -1,5 +1,27 @@
 <?php
 session_start();
+
+if (!$_SESSION['user']||$_SESSION['user']=="") {
+    die("Unauthorized request or login session has expired!");
+}
+if (substr($_SESSION['perms'],6,1)!=="1") {
+    echo 'Insufficient permission!';
+    exit();
+}
+
+if (empty($_GET['name'])) {
+    die("Name not specified.");
+}
+if (empty($_GET['uuid'])) {
+    die("UUID not specified.");
+}
+if (!preg_match('/[\w\s\.\-]+/', $_GET['name'])) {
+    die("Malformed name");
+}
+if (strlen($_GET['uuid'])!==36||!preg_match('/[a-fA-F0-9\-]+/', $_GET['uuid'])) {
+    die("Malformed UUID");
+}
+
 require_once('./configuration.php');
 global $config;
 if (empty($config)) {
@@ -9,20 +31,6 @@ if (empty($config)) {
 require_once("db.php");
 $db=new Db;
 $db->connect();
-
-if (empty($_GET['name'])) {
-    die("Name not specified.");
-}
-if (empty($_GET['uuid'])) {
-    die("UUID not specified.");
-}
-if (!$_SESSION['user']||$_SESSION['user']=="") {
-    die("Unauthorized request or login session has expired!");
-}
-if (substr($_SESSION['perms'],6,1)!=="1") {
-    echo 'Insufficient permission!';
-    exit();
-}
 
 $db->execute("INSERT INTO clients(`name`,`UUID`) VALUES ('".$db->sanitize($_GET['name'])."', '".$db->sanitize($_GET['uuid'])."')");
 
