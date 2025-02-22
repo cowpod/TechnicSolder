1537b6d262e607aaafdb22e30479a753dc026aa2
Henry Gross-Hellsen <6283258+cowpod@users.noreply.github.com>
2025-02-21 22:04:41 -0800

    change-clients-build.php: sanitize user input

--- functions/change-clients-build.php
+++ functions/change-clients-build.php
@@ -1,20 +1,32 @@
 <?php
 session_start();
-require_once('./configuration.php');
-global $config;
-if (empty($config)) {
-    $config=new Config();
-}
 
-if (empty($_GET['id'])) {
-    die("Modpack not specified.");
-}
 if (!$_SESSION['user']||$_SESSION['user']=="") {
     die("Unauthorized request or login session has expired!");
 }
 if (substr($_SESSION['perms'], 1, 1)!=="1") {
     echo 'Insufficient permission!';
     exit();
+}
+
+if (empty($_GET['id'])) {
+    die("Id (modpack) not specified.");
+}
+if (empty($_GET['client'])) {
+    die("Client(s) not specified");
+}
+if (is_numeric($_GET['id'])) {
+    die("Malformed id");
+}
+if (!preg_match('/^[a-fA-F0-9\-,]+$/', $_GET['client'])) {
+    // we'll check each uuid later
+    die ("Malformed clients (UUIDs seperated by comma)");
+}
+
+require_once('./configuration.php');
+global $config;
+if (empty($config)) {
+    $config=new Config();
 }
 
 global $db;
@@ -25,7 +37,15 @@ if (!isset($db)){
 }
 
 $clients = implode(",", $_GET['client']);
-$db->execute("UPDATE `builds` SET `clients` = '".$db->sanitize($clients)."' WHERE `id`=".$_GET['id']);
 
-header("Location: ".$config->get('dir')."build?id=".$_GET['id']);
+// check uuids
+for ($clients as $client) {
+    if (strlen($client)!==36||!preg_match('/^[a-fA-F0-9\-]+$/', $client)) {
+        die("Malformed client UUID: '{$client}'");
+    }
+}
+
+$db->execute("UPDATE `builds` SET `clients` = '{$clients}' WHERE `id` = {$_GET['id']}");
+
+header("Location: {$config->get('dir')}build?id={$_GET['id']}");
 exit();
